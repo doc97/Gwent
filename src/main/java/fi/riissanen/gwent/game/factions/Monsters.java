e9bb70fa7ed3c6c83952a755dd50958f748a23ee
doc97 <daniel.riissanen@gmail.com>
2016-12-04 00:00:37 +0200

    Fix random unit selection from empty row
    Bug
    Description: When Monsters ability activates it chooses a random
    row and saves one random unit from that row. The problem arises
    when the row is empty and the random unit index must be between 0
    and 0 (exclusive).
    Reproduce: Whenever the Monster's ability is activated without any
    units on the board.
    Fix: Now it is randomly choosing rows and checking if they have at
    least one unit and if so, stop checking and save a random unit from
    the row. If all rows have been checked and no row has at least one
    unit then no unit will be saved.

--- src/main/java/fi/riissanen/gwent/game/factions/Monsters.java
+++ src/main/java/fi/riissanen/gwent/game/factions/Monsters.java
@@ -5,8 +5,11 @@ import fi.riissanen.gwent.game.Player;
 import fi.riissanen.gwent.game.cards.abilities.Ability;
 import fi.riissanen.gwent.game.combat.CombatRow;
 import fi.riissanen.gwent.game.combat.Unit;
+import fi.riissanen.gwent.game.combat.UnitType;
 import fi.riissanen.gwent.game.events.Event;
 import fi.riissanen.gwent.game.events.RoundEndEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -23,11 +26,27 @@ public class Monsters extends Faction {
             @Override
             public void activate(GameSystem system) {
                 Random rand = new Random();
-                int rowIndex = rand.nextInt(3);
-                CombatRow row = system.getBoard().getRows(player.isFriendly())[rowIndex];
-                int unitIndex = rand.nextInt(row.getUnitCount());
-                Unit unit = row.getUnits().get(unitIndex);
-                system.getBoard().saveUnit(unit);
+                List<Integer> indices = new ArrayList<>();
+                for (int i = 0; i < UnitType.values().length; i++) {
+                    indices.add(i);
+                }
+                int rowIndex;
+                CombatRow row;
+                do {
+                    rowIndex = rand.nextInt(indices.size());
+                    row = system.getBoard().getRows(player.isFriendly())[rowIndex];
+                    if (row.getUnitCount() > 0) {
+                        break;
+                    } else {
+                        indices.remove(rowIndex);
+                    }
+                } while (indices.size() > 0);
+                
+                if (indices.size() > 0) {
+                    int unitIndex = rand.nextInt(row.getUnitCount());
+                    Unit unit = row.getUnits().get(unitIndex);
+                    system.getBoard().saveUnit(unit);
+                }
             }
 
             @Override
